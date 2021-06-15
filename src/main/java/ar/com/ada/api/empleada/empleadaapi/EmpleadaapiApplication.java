f4202d7952cafc29d56dc046cbedddfbcbe8d6e9
AgosBC <agostinabarello@gmail.com>
2021-06-15 19:11:03 -0300

    first commit

---
+++ src/main/java/ar/com/ada/api/empleada/empleadaapi/EmpleadaapiApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.empleada.empleadaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ada/api/empleada/empleadaapi/EmpleadaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.empleada.empleadaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
