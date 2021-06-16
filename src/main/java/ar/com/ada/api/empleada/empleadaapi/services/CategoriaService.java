335624831eb4c2785a1b645998a8dcfcba8e93f1
AgosBC <agostinabarello@gmail.com>
2021-06-16 11:39:51 -0300

    add categoria service incompleto

--- src/main/java/ar/com/ada/api/empleada/empleadaapi/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/empleada/empleadaapi/services/CategoriaService.java
@@ -1,5 +1,41 @@
 package ar.com.ada.api.empleada.empleadaapi.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.empleada.empleadaapi.entities.*;
+import ar.com.ada.api.repos.*;
+
+
+@Service
 public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepo;
+
+    public void crearCategoria(Categoria categoria){ //void o boolenan
+
+        categoriaRepo.save(categoria);
+    }
     
+
+
+
+    // para acortar codigo sera que se usa
+   /* public void grabar(Categoria categoria){
+        categoriaRepo.save(categoria)
+    }*/
+    // entonces en el metodo de arriba en la linea 18 iria grabar(categoria) y si es boolean un return true
+
+
+    public List<Categoria> traerCategorias(){
+        return categoriaRepo.findAllOrderByName();
+    }
+
+
+
+
+
 }
